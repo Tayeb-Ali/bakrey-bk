4479098a25d182413aa33c6fc65564584fc5fb9e
Elteyab Hassan <Elteyab.Hassan@gmail.com>
2020-12-20 17:09:59 +0200

    many Update in View

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
+         \App\Http\Middleware\TrustHosts::class,
         TrustProxies::class,
         HandleCors::class,
         CheckForMaintenanceMode::class,
